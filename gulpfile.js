b8f9bbe203efdb3b8dc636fddb36d81da67ba51b
M. Padiernos <mike.padiernos@gmail.com>
2021-10-04 23:07:12 -0400

    build(setup): libraries
    - removed jeet, rupture, typographic
    - replaced with bookcase, koffie, rfs, typesettings
    - seperated imports to imports.styl

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,18 @@ const 	{ 	src,
 			// parallel,
 			series,
 			task,
-			watch	}	= require('gulp');
+			watch	}	= require('gulp')
 
 const		auto		= require('autoprefixer'),
 			color		= require('postcss-colorblind'),
 			cssnano		= require('cssnano'),
-			// magic		= require('postcss-font-magician'),
-			jeet		= require('jeet'),
+			magic		= require('postcss-font-magician'),
 			maps		= require('gulp-sourcemaps'),
 			post		= require('gulp-postcss'),
-			querist		= require('mantis-querist'),
-			rename		= require("gulp-rename"),
+			rename		= require('gulp-rename'),
 			stylus		= require('gulp-stylus'),
 			// svgo		= require('postcss-svgo'),
-			typo		= require('typographic'),
-			ugly		= require('gulp-terser-js');
+			ugly		= require('gulp-terser-js')
 
 const path	= {
 	styles: {
@@ -30,28 +27,25 @@ const path	= {
 	}
 }
 
-const processors = [
-	auto,
-	color,
-	// magic,
-	// svgo,
-	cssnano,
-];
-
 function styles () {
-		return src(path.styles.src)
-			.pipe(maps.init())
-			.pipe(stylus({
-				use: [
-					jeet(),
-					querist(),
-					typo(),
-				]
-			}))
-			.pipe(post(processors))
-			.pipe(rename('wellspring.css'))
-			.pipe(maps.write('./'))
-			.pipe(dest(path.styles.dest));
+	const processors = [
+		auto,
+		color,
+		magic,
+		// svgo,
+		cssnano,
+	]
+	return src(path.styles.src)
+		.pipe(maps.init())
+		.pipe(stylus({
+			use: [
+
+			]
+		}))
+		.pipe(post(processors))
+		.pipe(rename('wellspring.css'))
+		.pipe(maps.write('./'))
+		.pipe(dest(path.styles.dest));
 }
 
 function scripts () {
